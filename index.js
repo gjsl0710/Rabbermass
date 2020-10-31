01d8f700ef77a5796e4d36034f17d0b28ca4ffbe
gjsl0710 <gjsl070@ruu.kr>
2020-10-31 14:06:47 +0900

    add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '.dm' }, status: 'online' })
+  client.user.setPresence({ LISTENING: { name: '.dm' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
